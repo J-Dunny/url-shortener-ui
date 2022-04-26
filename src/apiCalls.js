4e7383d6db1de5e760c9a5445d989d6033a9ec9d
Jake Dunafon <jlad795@gmail.com>
2022-04-26 09:08:20 -0700

    Add urltoshorten

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,7 +7,7 @@ const postUrls = (newUrl) => {
   return fetch('http://localhost:3001/api/v1/urls', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(newURL)
+    body: JSON.stringify(newUrl)
   })
     .then(response => response.json())
 }
--- src/components/UrlForm/UrlForm.js
+++ src/components/UrlForm/UrlForm.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 class UrlForm extends Component {
   constructor(props) {
-    super();
-    this.props = props;
+    super(props);
     this.state = {
       title: '',
       urlToShorten: ''
@@ -38,8 +37,8 @@ class UrlForm extends Component {
         <input
           type='text'
           placeholder='URL to Shorten...'
-          name='title'
-          value={this.state.title}
+          name='urlToShorten'
+          value={this.state.urlToShorten}
           onChange={e => this.handleNameChange(e)}
         />
 
