89ab25a6880fa81f17e1b7a27e4c1f83d5c312d7
Jake Dunafon <jlad795@gmail.com>
2022-04-26 11:09:25 -0700

    Add cypress test for post

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -50,14 +50,16 @@ describe("Home page display and userflow", () => {
         cy.intercept('POST', "http://localhost:3001/api/v1/urls", 
         
             {   id: 25, 
-                long_url: 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80', 
-                short_url: 'http://localhost:3001/useshorturl/1', 
+                long_url: 'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=803452345324', 
+                short_url: 'http://localhost:3001/useshorturl/13453454', 
                 title: 'Awesome photo 2'
             }
         
         ).as("post-urls")
         .get('button').click()
-        cy.wait('@post-urls')
+        .get('.url').contains("Awesome photo 2")
+        .get('.url').contains("http://localhost:3001/useshorturl/13453454")
+        .get('.url').contains("https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=803452345324")
         
         
     })
